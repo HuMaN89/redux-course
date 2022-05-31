aea52e8db39df91c8d8aff0307fcabf5504e67be
Kudabaev Bekzhan <human19091989@gmail.com>
2022-05-31 16:22:07 +0600

    in js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-import './styles.css'
+import "./styles.css";
+
+const addBtn = document.querySelector("#add"),
+  subBtn = document.querySelector("#sub"),
+  asBtn = document.querySelector("#async"),
+  counter = document.querySelector("#counter"),
+  theme = document.querySelector("#theme");
+
+counter.innerHTML = 0;
+
+function add() {
+  return (counter.innerHTML = +counter.innerHTML + 1);
+}
+function del() {
+  return (counter.innerHTML = +counter.innerHTML - 1);
+}
+function as() {
+  return (counter.innerHTML = Math.round(Math.random() * 10));
+}
+
+addBtn.addEventListener("click", add);
+subBtn.addEventListener("click", del);
+asBtn.addEventListener("click", as);
+theme.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
